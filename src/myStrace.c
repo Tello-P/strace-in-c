8ce840d7fdd26de1153d900e30ac6a926b33916f
Tello-P <perezgarrote.tello@gmail.com>
2026-02-17 18:47:17 +0100

    style: better output

--- src/myStrace.c
+++ src/myStrace.c
@@ -4,7 +4,7 @@
 #include <sys/types.h>
 #include <sys/ptrace.h>
 #include <sys/wait.h>
- #include <sys/user.h>
+#include <sys/user.h>
 #include <sys/syscall.h>
 #include <unistd.h>
 #include "../include/syscall_names_x86_64.h"
@@ -23,7 +23,7 @@ char* read_data(pid_t pid, unsigned long regAddr){
       printf("\nWARNING: ptrace_peekdata returned -1\n");
       break;
     }
-    
+
     char *aux = (char *)&buffer; // convert to char
 
     for (int i=0; i<8; i++){
@@ -34,43 +34,36 @@ char* read_data(pid_t pid, unsigned long regAddr){
       }
     }
   }
-    return data;
+  return data;
 }
 
 
 int main(){
-
-
   pid_t child_pid;
 
   /* CHILD PROCESS */
   if ((child_pid = fork()) == 0){
-    printf("hello from the child\n");
-    printf("This is the PID: %d\n", getpid());
     ptrace(PTRACE_TRACEME, 0, NULL, NULL);  // PID, ADDR and DATA are ignored if TRACEME 
     char *args[] = {"/bin/ls", "-l", "/home", NULL};  // REMOVE THIS WITH REAL ARGS
-    
+
     // execvp(program route, args)
     execvp(args[0], args);
 
     perror("execvp error");
     exit(EXIT_FAILURE);
-}
+  }
   else if(child_pid<0){
     perror("fork error");
     return -1;
   }
   /* FATHER PROCESS */
   else{
-    printf("hello from the parent\n");
-    printf("This is the PID: %d\n", getpid());
 
     int state;
     int entry=1;// ALWAYS START AT ONE
     char *syscallName;
 
     waitpid(child_pid, &state, 0);
-  
 
     struct user_regs_struct regs; //struct to get regs defined in user.h  
     ptrace(PTRACE_SETOPTIONS, child_pid, 0, PTRACE_O_TRACESYSGOOD); // better syscall tracking
@@ -80,47 +73,41 @@ int main(){
         perror("ptrace exited");
         break;
       }  // Makes the tracee advance till the next stop
-      /////////////////////////////////////////////////////////////
+
       waitpid(child_pid, &state, 0);
       if (WIFEXITED(state)){
         printf("Process exited\n");
         return 1;
       }
-      /////////////////////////////////////////////////////////////
- 
 
       if (ptrace(PTRACE_GETREGS, child_pid, NULL, &regs) == -1) {
         perror("ptrace(PTRACE_GETREGS)");
         break;
       }
-    
+
       if (entry) {
-            syscallName = get_syscall_name(regs.orig_rax);
-            // SYSCALL ENTRY: orig_rax is the ID
-            printf("Syscall: %-15s (ID: %3llu) ",
-                   syscallName, regs.orig_rax);
-            entry = 0; 
+        syscallName = get_syscall_name(regs.orig_rax);
+        // SYSCALL ENTRY: orig_rax is the ID
+        printf("Syscall: %-15s (ID: %3llu) ",
+               syscallName, regs.orig_rax);
+        entry = 0; 
+      } else {
+        // SYSCALL EXIT: rax is the Result
+        if ((long long)regs.rax < 0 && (long long)regs.rax > -4096) {
+          printf(" | Result: ERROR (%lld)\n", (long long)regs.rax);
         } else {
-            // SYSCALL EXIT: rax is the Result
-            if ((long long)regs.rax < 0 && (long long)regs.rax > -4096) {
-                printf(" | Result: ERROR (%lld)\n", (long long)regs.rax);
-            } else {
-                printf(" | Result: 0x%llx\n", regs.rax);
-                // returning the data
-                if (strcmp(syscallName, "openat")==0){
-                char *dataString = read_data(child_pid, regs.rsi);
-                printf("DATA: %s\n", dataString);
+          printf(" | Result: 0x%llx\n", regs.rax);
+          // returning the data
+          if (strcmp(syscallName, "openat")==0 || (strcmp(syscallName, "write")==0) || strcmp(syscallName, "read")==0){
+            char *dataString = read_data(child_pid, regs.rsi);
+            printf("\n---------------\n");
+            printf("DATA: %s\n", dataString);
+            printf("-----------------\n");
           }
-            }
-            entry = 1;
         }
-
-
+        entry = 1;
+      }
     }
-     
-    }
-
-
-
+  }
   return 0;
 }
